87c9c43dfd6ea586ee63c66ae38fbf4b743c6867
Nayan Khedkar <nayan.khedkar@exultcorp.com>
2017-01-24 10:49:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var database = require('./database').init();
 var webserver = require('./webserver').init();
-
+ console.log('in index.js');
 var sync = database.Package.sync().then( function () {
+    console.log('databasesync()');
     return database.onSync.bind(database);
 }).then(function () {
+    console.log('server started');
     webserver.app.set('pkg', database.Package);
     webserver.listen(process.env.PORT || 5000);
 });
